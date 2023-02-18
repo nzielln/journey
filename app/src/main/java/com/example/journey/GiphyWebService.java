30306ece5ca76c356c0b203f8d965d13e5907193
tinashemazaiwana <tzambe@aol.com>
2023-02-17 21:47:54 -0500

    Updated new activity. - Tinashe Mazaiwana

--- app/src/main/java/com/example/journey/GiphyWebService.java
+++ app/src/main/java/com/example/journey/GiphyWebService.java
@@ -3,14 +3,26 @@ package com.example.journey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GiphyWebService extends AppCompatActivity {
+
+  private EditText urlEditText;
+  private TextView titleTextView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_giphy_web_service);
+
+    urlEditText = (EditText)findViewById(R.id.url_editText);
+    titleTextView = (TextView) findViewById(R.id.webService);
+
   }
 
+  /**
+   * The WebServiceButtonHandler() method
+   */
 
 }
