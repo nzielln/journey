23e0c346c063615d00c926d627b9549511e47178
Ellah Nzikoba <ellahnzikoba@gmail.com>
2023-03-04 09:43:46 -0500

    add new entry to db when user creates account

--- app/src/main/java/com/example/journey/Sticker/Constants.java
+++ app/src/main/java/com/example/journey/Sticker/Constants.java
@@ -5,19 +5,11 @@ import android.graphics.drawable.Drawable;
 import com.example.journey.R;
 
 public class Constants {
-    public static String PASSWORD_RESULT_KEY = "PASSWORD_RESULT_KEY";
-    public static String EMAIL_RESULT_KEY = "EMAIL_RESULT_KEY";
-    public static String SHOW_CREATE_RESULT_KEY = "SHOW_CREATE_RESULT_KEY";
-    public static String PASSWORD_REQUEST_KEY = "PASSWORD_REQUEST_KEY";
-    public static String EMAIL_REQUEST_KEY = "EMAIL_REQUEST_KEY";
-    public static String SHOW_CREATE_REQUEST_KEY = "SHOW_CREATE_REQUEST_KEY";
-    public static String PASSWORD_KEY = "PASSWORD_KEY";
-    public static String EMAIL_KEY = "EMAIL_KEY";
     public static String DELEGATE = "DELEGATE";
 
     public static String ERROR_SIGNING_IN_MESSAGE = "There was an error signing you in. Please try again.";
     public static String ERROR_CREATING_ACCOUNT_MESSAGE = "There was an error creating your account. Please try again.";
-    public static String SIGN_OUT_MESSAGE = "You've successfully been loggeg out.";
+    public static String USERS_DATABASE_ROOT = "users";
 
     public final static String STICKER_ANGRY = "angry";
     public final static String STICKER_BORING = "boring";
