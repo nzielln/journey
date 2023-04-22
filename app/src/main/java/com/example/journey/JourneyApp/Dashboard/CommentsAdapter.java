3199b42a7a0701b64e43beb9b4789120fe192387
netti <welshnn@outlook.com>
2023-04-21 21:12:32 -0400

    adapter

--- app/src/main/java/com/example/journey/JourneyApp/Dashboard/CommentsAdapter.java
+++ app/src/main/java/com/example/journey/JourneyApp/Dashboard/CommentsAdapter.java
@@ -8,11 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.journey.JourneyApp.Main.Database;
 import com.example.journey.R;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-
-import org.w3c.dom.Text;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -26,16 +26,19 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.ViewHo
         this.commentItems = commentItems;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder){
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView commenterUserName;
         private TextView commentDate;
         private TextView commentContext;
 
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
 
-    }
-    public ViewHolder(@NonNull View itemView){
-        super(itemView);
+            commenterUserName = (TextView)itemView.findViewById(R.id.name_comment_Tv);
+            commentDate = (TextView)itemView.findViewById(R.id.date_comment_Tv);
+            commentContext = (TextView)itemView.findViewById(R.id.content_comment_Tv);
 
+        }
     }
 
     @NonNull
@@ -43,12 +46,20 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.ViewHo
     public CommentsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.fragment_dashboard_comments,parent,false);
-        ViewHolder viewHolder = new ViewHolder();
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull CommentsAdapter.ViewHolder holder, int position) {
+        dbReference = FirebaseDatabase.getInstance(Database.JOURNEYDB).getReference();
+        currentUser = Database.FIREBASE_AUTH.getCurrentUser();
+
+        CommentsModel comment = commentItems.get(position);
+        holder.commenterUserName.setText(comment.getCommenterName());
+        holder.commentContext.setText(comment.getCommentContent());
+        holder.commentDate.setText(comment.getCommentDate());
+
 
     }
 
