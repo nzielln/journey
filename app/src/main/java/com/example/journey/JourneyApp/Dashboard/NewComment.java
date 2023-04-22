31893a18b2e22b40f3320a2d1b7147c22583ed8a
netti <welshnn@outlook.com>
2023-04-22 17:40:59 -0400

    retrieve comments from db

--- app/src/main/java/com/example/journey/JourneyApp/Dashboard/NewComment.java
+++ app/src/main/java/com/example/journey/JourneyApp/Dashboard/NewComment.java
@@ -1,34 +1,31 @@
 package com.example.journey.JourneyApp.Dashboard;
 
+import com.example.journey.JourneyApp.Profile.Models.UserModel;
+import com.google.firebase.storage.StorageReference;
+
 public class NewComment {
-    private String commentId;
+
     private String commentUserId;
-    private String commentUserName;
+    private String commentId;
     private String commentContent;
     private String commentDate;
 
+   public NewComment(){}
 
-    public NewComment(String commentId, String commentUserId,String commentDate,
-                      String commentContent) {
+    public NewComment(String commentId, String commentUserId, String commentDate, String commentContent) {
         this.commentId = commentId;
         this.commentUserId = commentUserId;
         this.commentDate = commentDate;
         this.commentContent = commentContent;
     }
-    public NewComment(String commentId, String commentUserId,String commentDate,
-                      String commentUserName, String commentContent) {
-        this.commentId = commentId;
-        this.commentUserName = commentUserName;
-        this.commentUserId = commentUserId;
-        this.commentDate = commentDate;
-        this.commentContent = commentContent;
+
+    public String getCommentUserId() {
+        return commentUserId;
     }
 
     public String getCommentId() {
         return commentId;
     }
-    public String getCommentUserId(){return commentUserId;}
-    public String getCommentUserName(){return commentUserName;}
     public String getCommentDate(){
         return commentDate;
     }
